ac70679e94f85aa1b5d267b7a4b5dbae96202339
Duc <vanduc2514@gmail.com>
2020-07-06 23:50:39 +0700

    Add Validator for register form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TemplateDrivenFormComponent } from './template-driven-form/template-driven-form.component';
+import {FormsModule} from '@angular/forms';
+import {ReactiveFormsModule} from '@angular/forms';
+import { RegisterComponent } from './register/register.component';
+import { RegisterFormBuilderComponent } from './register-form-builder/register-form-builder.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TemplateDrivenFormComponent,
+    RegisterComponent,
+    RegisterFormBuilderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
